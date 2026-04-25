7c3b96f49c79561cf461cda6f2f3b36957ea9ce9
Fouad Najeh <cntact.watchandearn@outlook.com>
2026-04-25 19:37:05 +0000

    Fix: Reconstruct android/app/build.gradle and MainActivity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fouad.watch_earn"
+    namespace = "com.example.watch_earn"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.fouad.watch_earn"
+        applicationId = "com.example.watch_earn"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/fouad/watchandearn/MainActivity.kt
+++ android/app/src/main/kotlin/com/fouad/watchandearn/MainActivity.kt
@@ -1,6 +1,4 @@
 package com.fouad.watchandearn
-
 import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity: FlutterActivity() {
 }
